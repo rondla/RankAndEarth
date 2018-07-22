a0197734ffb407d751361d8eaa72daeecfd49792
rondla <saikiran.rondla@gmail.com>
2018-07-23 01:11:00 +0530

    Initial Commit

---
+++ EarthAndRank/src/main/java/HackerRankEarth/EarthAndRank/App.java
@@ -0,0 +1,13 @@
+package HackerRankEarth.EarthAndRank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
